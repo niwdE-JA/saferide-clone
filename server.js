80e09552654edef1f9c53354b85383d045f96256
Edwin-A.J <dezwhy97@gmail.com>
2025-07-10 01:19:08 +0100

    added input vlidation on signup

--- server.js
+++ server.js
@@ -29,21 +29,21 @@ apiRouter.get('/login', (req, res) => {
 
 
 // define validators
-email_validator = body('email')
+const email_validator = body('email')
     .isEmail().withMessage('Please enter a valid email address.')
     .normalizeEmail()
 
-password_validator = body('password')
+const password_validator = body('password')
     .isLength({ min: 8, max: 12 }).withMessage('Password must be at least between 8 and 12 characters long.')
     .matches(/\d/).withMessage('Password must contain at least one number.')
     // .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character.'), // Must contain a special character
 
-firstname_validator = body('firstname')
+const firstname_validator = body('firstname')
       .trim()
       .notEmpty().withMessage('First name is required.')
     //   .isLength({ min: 3, max: 20 }).withMessage('First name must be between 3 and 20 characters.')
 
-lastname_validator = body('lastname')
+const lastname_validator = body('lastname')
       .trim()
       .notEmpty().withMessage('Last name is required.')
     //   .isLength({ min: 3, max: 20 }).withMessage('Last name must be between 3 and 20 characters.')
@@ -74,10 +74,10 @@ app.post(
     res.status(201).json({
       message: 'User signed up successfully!',
       user: {
-        username,
         email,
         firstname,
-        lastname
+        lastname,
+        password
       }
     });
   }
