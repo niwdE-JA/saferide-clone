0f3a9143b9ef1fd31a3b2971b6cba743f152cc1d
Edwin-A.J <dezwhy97@gmail.com>
2025-07-10 01:15:51 +0100

    added validation of signup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express, { Router, json } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 const apiRouter = Router()
@@ -25,6 +26,62 @@ apiRouter.get('/login', (req, res) => {
     items: ['item1', 'item2', 'item3']
   });
 });
+
+
+// define validators
+email_validator = body('email')
+    .isEmail().withMessage('Please enter a valid email address.')
+    .normalizeEmail()
+
+password_validator = body('password')
+    .isLength({ min: 8, max: 12 }).withMessage('Password must be at least between 8 and 12 characters long.')
+    .matches(/\d/).withMessage('Password must contain at least one number.')
+    // .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('Password must contain at least one special character.'), // Must contain a special character
+
+firstname_validator = body('firstname')
+      .trim()
+      .notEmpty().withMessage('First name is required.')
+    //   .isLength({ min: 3, max: 20 }).withMessage('First name must be between 3 and 20 characters.')
+
+lastname_validator = body('lastname')
+      .trim()
+      .notEmpty().withMessage('Last name is required.')
+    //   .isLength({ min: 3, max: 20 }).withMessage('Last name must be between 3 and 20 characters.')
+
+
+
+app.post(
+  '/signup',
+  [ email_validator, password_validator, firstname_validator, lastname_validator ],
+  (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // If there are validation errors, return a 400 Bad Request with the errors
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // If validation passes, process the signup data
+    const { email, password, firstname, lastname } = req.body;
+
+
+    console.log('Signup Data Received:');
+    console.log(`Email: ${email}`);
+    console.log(`Firstname: ${firstname}`);
+    console.log(`Lastname: ${lastname}`);
+
+    res.status(201).json({
+      message: 'User signed up successfully!',
+      user: {
+        username,
+        email,
+        firstname,
+        lastname
+      }
+    });
+  }
+);
 
 
 // Run Server
