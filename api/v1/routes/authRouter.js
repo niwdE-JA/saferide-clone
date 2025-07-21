7b1cd2b1587f4d127371fd86ae6445629be583b9
Edwin-A.J <dezwhy97@gmail.com>
2025-07-21 06:27:02 +0100

    added contact method on signup

--- api/v1/routes/authRouter.js
+++ api/v1/routes/authRouter.js
@@ -77,7 +77,7 @@ authRouter.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password, firstname, lastname } = req.body;
+    const { email, password, firstname, lastname, contact_method } = req.body;
 
     try {
       const db = req.firestoreDatabase;
@@ -107,6 +107,7 @@ authRouter.post(
         password: hashedPassword,
         createdAt: FieldValue.serverTimestamp(), // Timestamp for creation
         otp: otp,
+        contact_method,
         otpExpiry: Timestamp.fromDate(otpExpiry)
 
       });
